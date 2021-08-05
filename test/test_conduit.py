86cc8fba4bd8fad05302a688d951e6d8e17be87c
PappIldiko <pappildiko007@gmail.com>
2021-08-05 19:52:33 +0200

    Conduit allure, yml push

--- test/test_conduit.py
+++ test/test_conduit.py
@@ -66,18 +66,18 @@ class TestConduit(object):
         )
         assert element.text == "Your Feed"
 
-    #
+
     # #     # Test3 log out
     def test_logout(self):
         conduit_registration(self.driver)
         element = WebDriverWait(
-            self.driver, 9).until(
-            EC.visibility_of_element_located((By.XPATH, ('//a[@active-class="active"]')))
+            self.driver, 15).until(
+            EC.visibility_of_element_located((By.XPATH, ('//ul/li[5]')))
         )
         element.click()
         # self.driver.find_element_by_xpath('//a[@active-class="active"]').click()
 
-    #     # logoutot kell assertezni?
+    #     # logoutot kell assertezni? igen
     #
     #
     # # Test4 create and delete new article
@@ -102,19 +102,19 @@ class TestConduit(object):
     #     element.click()
 
     # Test5 create new article
-    def test_create_new_article(self):
-        conduit_registration(self.driver)
-        time.sleep(3)
-        self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
-        time.sleep(2)
-        self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(
-            "Chocolate lollipop oat cake")
-        self.driver.find_elements_by_xpath('//form//input')[1].send_keys("About cakes")
-        self.driver.find_element_by_xpath(
-            '//form//textarea[@placeholder="Write your article (in markdown)"]').send_keys(
-            "Powder donut liquorice I love I love powder sesame snaps jujubes. Gummies chocolate sweet roll. Icing I love powder I love danish cookie I love. Cake chocolate bar I love. Cupcake I love cheesecake pastry I love fruitcake candy croissant. Lollipop caramels I love bonbon. Gingerbread powder macaroon cookie. Sesame snaps tootsie roll bear claw I love. Brownie cake gingerbread carrot cake marshmallow I love halvah.")
-        self.driver.find_elements_by_xpath('//form//input')[2].send_keys("bonbon")
-        self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
+    # def test_create_new_article(self):
+    #     conduit_registration(self.driver)
+    #     time.sleep(3)
+    #     self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
+    #     time.sleep(2)
+    #     self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(
+    #         "Chocolate lollipop oat cake")
+    #     self.driver.find_elements_by_xpath('//form//input')[1].send_keys("About cakes")
+    #     self.driver.find_element_by_xpath(
+    #         '//form//textarea[@placeholder="Write your article (in markdown)"]').send_keys(
+    #         "Powder donut liquorice I love I love powder sesame snaps jujubes. Gummies chocolate sweet roll. Icing I love powder I love danish cookie I love. Cake chocolate bar I love. Cupcake I love cheesecake pastry I love fruitcake candy croissant. Lollipop caramels I love bonbon. Gingerbread powder macaroon cookie. Sesame snaps tootsie roll bear claw I love. Brownie cake gingerbread carrot cake marshmallow I love halvah.")
+    #     self.driver.find_elements_by_xpath('//form//input')[2].send_keys("bonbon")
+    #     self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
 
         # element = WebDriverWait(
         #     self.driver, 5).until(
