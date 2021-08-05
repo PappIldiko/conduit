65f53c4e23cf1638447b7888bb2a32e288f69df1
PappIldiko <pappildiko007@gmail.com>
2021-08-05 17:26:04 +0200

    Conduit allure, yml push

---
+++ test/conduit_data.py
@@ -0,0 +1,11 @@
+import time
+
+
+def conduit_registration(driver):
+
+    driver.find_element_by_xpath('//a[@href="#/register"]').click()
+    driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys("Ildiko123@gmail.com")
+    driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("Ildiko123@gmail.com")
+    driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Ildiko123@gmail.com")
+    time.sleep(3)
+    driver.find_element_by_xpath('//button').click()
