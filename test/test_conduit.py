afd7ffb40e41192d41ee741717749da00864e842
PappIldiko <pappildiko007@gmail.com>
2021-08-06 15:14:59 +0200

    Conduit allure, yml push

--- test/test_conduit.py
+++ test/test_conduit.py
@@ -19,73 +19,74 @@ class TestConduit(object):
     def teardown(self):
         self.driver.quit()
 
-    # # # Test1 - oldal megjelenése ok
-    # def test_home_page_appearances(self):
-    #     assert self.driver.find_element_by_xpath("//h1").text == "conduit"
-
-    # Test1 - cookie-k
-
-    # # Test2 - regisztráció - rendben fut
-    # def test_registration(self):
-    #     random_number = str(random.randint(1000, 2000))
-    #     username = random_number + "Teszteles"
-    #     email = username + "@gmail.com"
-    #
-    #     self.driver.maximize_window()
-    #     self.driver.find_element_by_xpath('//a[@href="#/register"]').click()
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys(username)
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys(email)
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(username)
-    #     time.sleep(3)
-    #     self.driver.find_element_by_xpath('//button').click()
-    #     element = WebDriverWait(
-    #         self.driver, 5).until(
-    #         EC.visibility_of_element_located((By.XPATH, ('//div[text()="Your registration was successful!"]')))
-    #     )
-    #     assert element.text == "Your registration was successful!"
-
-    # # Test3 log in - ok
-    # def test_login(self):
-    #     conduit_registration(self.driver)
-    #     self.driver.find_element_by_xpath('//a[@href="#/login"]').click()
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("Ildiko123@gmail.com")
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Ildiko123@gmail.com")
-    #     # time.sleep(5)
-    #     # self.driver.find_element_by_xpath('//form/button').click()
-    #     elem = WebDriverWait(
-    #         self.driver, 5).until(
-    #         EC.visibility_of_element_located((By.XPATH, ('//form/button')))
-    #     )
-    #     elem.click()
-    #
-    #     element = WebDriverWait(
-    #         self.driver, 5).until(
-    #         EC.visibility_of_element_located((By.XPATH, ('//a[normalize-space(text())="Your Feed"]')))
-    #     )
-    #     assert element.text == "Your Feed"
+    # Test1 - oldal megjelenése és cookie-k elfogadása ok
+    def test_home_page_appearances(self):
+        assert self.driver.find_element_by_xpath("//h1").text == "conduit"
+        element = self.driver.find_element_by_xpath('//div[@id="cookie-policy-panel"]//button[2]')
+        element.click()
 
 
-    # # Test4 log out - ok
-    # def test_logout(self):
-    #     conduit_registration(self.driver)
-    #     element = WebDriverWait(
-    #         self.driver, 14).until(
-    #         EC.visibility_of_element_located((By.XPATH, ('//i[@class="ion-android-exit"]')))
-    #     )
-    #     element.click()
-    #     time.sleep(2)
-    #
-    #     elem = WebDriverWait(
-    #         self.driver, 5).until(
-    #         EC.visibility_of_element_located((By.XPATH, ('//a[normalize-space(text())="Sign in"]')))
-    #     )
-    #     assert elem.text == "Sign in"
+    # Test2 - regisztráció - rendben fut
+    def test_registration(self):
+        random_number = str(random.randint(1000, 2000))
+        username = random_number + "Teszteles"
+        email = username + "@gmail.com"
+
+        self.driver.maximize_window()
+        self.driver.find_element_by_xpath('//a[@href="#/register"]').click()
+        self.driver.find_element_by_xpath('//input[@placeholder="Username"]').send_keys(username)
+        self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys(email)
+        self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys(username)
+        time.sleep(3)
+        self.driver.find_element_by_xpath('//button').click()
+        element = WebDriverWait(
+            self.driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, ('//div[text()="Your registration was successful!"]')))
+        )
+        assert element.text == "Your registration was successful!"
+
+    # Test3 log in - ok
+    def test_login(self):
+        conduit_registration(self.driver)
+        self.driver.find_element_by_xpath('//a[@href="#/login"]').click()
+        self.driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys("Ildiko123@gmail.com")
+        self.driver.find_element_by_xpath('//input[@placeholder="Password"]').send_keys("Ildiko123@gmail.com")
+        # time.sleep(5)
+        # self.driver.find_element_by_xpath('//form/button').click()
+        elem = WebDriverWait(
+            self.driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, ('//form/button')))
+        )
+        elem.click()
+
+        element = WebDriverWait(
+            self.driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, ('//a[normalize-space(text())="Your Feed"]')))
+        )
+        assert element.text == "Your Feed"
 
 
-        # self.driver.find_element_by_xpath('//a[@active-class="active"]').click() ('//ul/li[5]')
+    # Test4 log out - ok
+    def test_logout(self):
+        conduit_registration(self.driver)
+        element = WebDriverWait(
+            self.driver, 14).until(
+            EC.visibility_of_element_located((By.XPATH, ('//i[@class="ion-android-exit"]')))
+        )
+        element.click()
+        time.sleep(2)
+
+        elem = WebDriverWait(
+            self.driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, ('//a[normalize-space(text())="Sign in"]')))
+        )
+        assert elem.text == "Sign in"
 
 
-    #
+        self.driver.find_element_by_xpath('//a[@active-class="active"]').click() ('//ul/li[5]')
+
+
+
     # # Test4 create and delete new article - ez nem kell
     # def test_create_delete_new_article(self):
     #     conduit_registration(self.driver)
@@ -108,50 +109,50 @@ class TestConduit(object):
     #     element.click()
 
     # Test5 create new article - ok
-    # def test_create_new_article(self):
-    #     conduit_registration(self.driver)
-    #     time.sleep(3)
-    #     self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
-    #     time.sleep(2)
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(
-    #         "Chocolate lollipop oat cake")
-    #     self.driver.find_elements_by_xpath('//form//input')[1].send_keys("About cakes")
-    #     self.driver.find_element_by_xpath(
-    #         '//form//textarea[@placeholder="Write your article (in markdown)"]').send_keys(
-    #         "Powder donut liquorice I love I love powder sesame snaps jujubes. Gummies chocolate sweet roll. Icing I love powder I love danish cookie I love. Cake chocolate bar I love. Cupcake I love cheesecake pastry I love fruitcake candy croissant. Lollipop caramels I love bonbon. Gingerbread powder macaroon cookie. Sesame snaps tootsie roll bear claw I love. Brownie cake gingerbread carrot cake marshmallow I love halvah.")
-    #     self.driver.find_elements_by_xpath('//form//input')[2].send_keys("bonbon")
-    #     self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
-    #
-    #     element = WebDriverWait(
-    #         self.driver, 5).until(
-    #         EC.visibility_of_element_located((By.XPATH, ('//button[@class="btn btn-outline-danger btn-sm"]')))
-    #     )
-    #     element.click()
+    def test_create_new_article(self):
+        conduit_registration(self.driver)
+        time.sleep(3)
+        self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(
+            "Chocolate lollipop oat cake")
+        self.driver.find_elements_by_xpath('//form//input')[1].send_keys("About cakes")
+        self.driver.find_element_by_xpath(
+            '//form//textarea[@placeholder="Write your article (in markdown)"]').send_keys(
+            "Powder donut liquorice I love I love powder sesame snaps jujubes. Gummies chocolate sweet roll. Icing I love powder I love danish cookie I love. Cake chocolate bar I love. Cupcake I love cheesecake pastry I love fruitcake candy croissant. Lollipop caramels I love bonbon. Gingerbread powder macaroon cookie. Sesame snaps tootsie roll bear claw I love. Brownie cake gingerbread carrot cake marshmallow I love halvah.")
+        self.driver.find_elements_by_xpath('//form//input')[2].send_keys("bonbon")
+        self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
+
+        element = WebDriverWait(
+            self.driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, ('//button[@class="btn btn-outline-danger btn-sm"]')))
+        )
+        element.click()
 
     # Test6 modify article
-    # def test_modify_article(self):
-    #     conduit_registration(self.driver)
-    #     time.sleep(3)
-    #     self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
-    #     time.sleep(2)
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(
-    #         "Chocolate lollipop oat cake")
-    #     self.driver.find_elements_by_xpath('//form//input')[1].send_keys("About cakes")
-    #     self.driver.find_element_by_xpath(
-    #         '//form//textarea[@placeholder="Write your article (in markdown)"]').send_keys(
-    #         "Powder donut liquorice I love I love powder sesame snaps jujubes. Gummies chocolate sweet roll. Icing I love powder I love danish cookie I love. Cake chocolate bar I love. Cupcake I love cheesecake pastry I love fruitcake candy croissant. Lollipop caramels I love bonbon. Gingerbread powder macaroon cookie. Sesame snaps tootsie roll bear claw I love. Brownie cake gingerbread carrot cake marshmallow I love halvah.")
-    #     self.driver.find_elements_by_xpath('//form//input')[2].send_keys("bonbon")
-    #     self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
-    #
-    #
-    #     time.sleep(2)
-    #     self.driver.find_element_by_xpath('//span[normalize-space(text()=" Edit Article")]').click()
-    #     self.driver.find_element_by_xpath('//a[@class="btn btn-sm btn-outline-secondary"]').click()
-    #     time.sleep(2)
-    #     # self.driver.find_element_by_xpath('//input[@class="form-control form-control-lg"]').send_keys(" modified")
-    #     self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(" modified")
-    #     time.sleep(2)
-    #     self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
+    def test_modify_article(self):
+        conduit_registration(self.driver)
+        time.sleep(3)
+        self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(
+            "Chocolate lollipop oat cake")
+        self.driver.find_elements_by_xpath('//form//input')[1].send_keys("About cakes")
+        self.driver.find_element_by_xpath(
+            '//form//textarea[@placeholder="Write your article (in markdown)"]').send_keys(
+            "Powder donut liquorice I love I love powder sesame snaps jujubes. Gummies chocolate sweet roll. Icing I love powder I love danish cookie I love. Cake chocolate bar I love. Cupcake I love cheesecake pastry I love fruitcake candy croissant. Lollipop caramels I love bonbon. Gingerbread powder macaroon cookie. Sesame snaps tootsie roll bear claw I love. Brownie cake gingerbread carrot cake marshmallow I love halvah.")
+        self.driver.find_elements_by_xpath('//form//input')[2].send_keys("bonbon")
+        self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
+
+
+        time.sleep(2)
+        self.driver.find_element_by_xpath('//span[normalize-space(text()=" Edit Article")]').click()
+        self.driver.find_element_by_xpath('//a[@class="btn btn-sm btn-outline-secondary"]').click()
+        time.sleep(2)
+        # self.driver.find_element_by_xpath('//input[@class="form-control form-control-lg"]').send_keys(" modified")
+        self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys(" modified")
+        time.sleep(2)
+        self.driver.find_element_by_xpath('//button[normalize-space(text()="Publish Article")]').click()
 
 
     # Test7 delete article
