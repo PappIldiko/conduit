f3e90ab2dbe29597176d33ea0fb7f10436d012a5
PappIldiko <pappildiko007@gmail.com>
2021-08-08 16:15:57 +0200

    Conduit allure, yml push

--- test/test_conduit.py
+++ test/test_conduit.py
@@ -168,7 +168,7 @@ class TestConduit(object):
     def test_listing_data(self):
         self.test_login()
         time.sleep(3)
-        self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[0].click()
+        self.driver.find_elements_by_xpath('//a[@class="nav-link"]')[1].click()
 
         time.sleep(3)
         self.driver.find_element_by_xpath('//input[@placeholder="Article Title"]').send_keys("Chocolate lollipop oat cake")
